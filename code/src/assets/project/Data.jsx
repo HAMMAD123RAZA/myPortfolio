86905d802de465c2b92fbd3b5fd1eefae91426a3
HAMMAD123RAZA <01hammadraza@gmail.com>
2023-09-07 13:50:03 -0700

    first commit

---
+++ code/src/assets/project/Data.jsx
@@ -0,0 +1,46 @@
+const Data=[
+    {
+        image:' grocey.jpg',
+        title:'Food Menu Web',
+        technologies:'Html , Css , Mui , React Js',
+        desc:'built with pure react js and filtered component for specific menu item',
+        height: '30vh'
+
+    },
+
+    {
+        image:'tour.jpg',
+        title:'Tour Web',
+        technologies:'Html , Css , Mui , React Js',
+        desc:'built with react js where crud operation is used in case if a user doesnt like a particular tour. At the end refreshing data functionality is  also given if items are emptied  ',
+        height: '30vh'
+
+    },
+
+    {
+        image: 'biography.jpg',
+        title: 'biography App',
+        technologies: 'Html , Bootstrap ,Javascript',
+        desc:'Made up with completely bootstrap .In this biography web ive introduced the mr beans whole life from his education to a tv show career ',
+        height: '30vh' // Add the desired height here
+    }
+    ,
+
+    {
+        image:'budget.jpg',
+        title:'Budget App',
+        technologies:'Html , Css ,Javascript',
+        desc:'Built with  html ,css and js .This budget app  includes javascript  whole lot functionality like a user can add and calculate  his expense and can see the overview and can also delete it  ',
+        height: '30vh'
+    } ,
+
+    {
+        image:'todo.jpg',
+        title:'Todo List App',
+        technologies:'Html , Css ,Javascript',
+        desc:'Created using HTML, CSS, and JavaScript, this Todo app incorporates CRUD operations. It utilizes DOM manipulation to enable these functions seamlessly.',
+        height: '30vh'
+    }
+   
+]
+export default Data
